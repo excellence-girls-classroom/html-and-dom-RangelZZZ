36c03104578e8b1523c85812de10eb4e6e9de3f4
RangelZZZ <18792561236@163.com>
2016-05-08 21:37:47 +0800

    addBootStrap

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function submit() {
     isEmpty();
 
     var score = statisticsScore();
-    var totalScore = document.getElementById("总分");
+    var totalScore = document.getElementById("totalScore");
 
     totalScore.value = score;
 }
@@ -11,9 +11,9 @@ function submit() {
 
 function isEmpty() {
 
-    var classInformation = document.getElementById("班级");
-    var studentNumber = document.getElementById("学号");
-    var studentName = document.getElementById("姓名");
+    var classInformation = document.getElementById("className");
+    var studentNumber = document.getElementById("studentNumber");
+    var studentName = document.getElementById("studentName");
 
     if (classInformation.value.length === 0) {
         alert("请输入班级");
@@ -44,7 +44,7 @@ function statisticsScore() {
 function countBlanksQuestionScore() {
 
     var blankAnswer = 0;
-    var answer = document.getElementsByName("答案");
+    var answer = document.getElementsByName("blanksAnswer");
 
     if (answer[0].value === "统一建模语言") {
         blankAnswer += 5;
@@ -66,7 +66,7 @@ function countBlanksQuestionScore() {
 function countChoiceQuestionScore() {
 
     var choiceAnswer = 0;
-    var answer = document.getElementsByName("选择");
+    var answer = document.getElementsByName("choiceAnswer");
 
     if (answer[0].value === "B") {
         choiceAnswer += 10;
@@ -83,7 +83,7 @@ function countChoiceQuestionScore() {
 function countMultipleChoiceQuestionScore() {
 
     var multipleChoiceAnwser = 0;
-    var answer = document.getElementsByName("多选");
+    var answer = document.getElementsByName("multipleChoiceAnswer");
 
     if (answer[0].value === "A" + "B" + "D") {
         multipleChoiceAnwser += 10;
@@ -99,7 +99,7 @@ function countMultipleChoiceQuestionScore() {
 function countTrueOrFalseQuestionScore() {
 
     var trueOrFalseAnswer = 0;
-    var answer = document.getElementsByName("判断");
+    var answer = document.getElementsByName("trueOrFalseAnswer");
 
     if (answer[0].value === "错误") {
         trueOrFalseAnswer += 10;
@@ -115,7 +115,7 @@ function countTrueOrFalseQuestionScore() {
 function countShortAnswerQuestionScore() {
 
     var shortAnswerAnswer = 0;
-    var answer = document.getElementById("简答");
+    var answer = document.getElementById("shortAnswerAnswer");
 
     if (answer.value === "模型是对现实世界的简化和抽象,模型是对所研究的系统、过程、事物或概念的一种表达形" +
         "式。可以是物理实体;可以是某种图形;或者是一种数学表达式") {
